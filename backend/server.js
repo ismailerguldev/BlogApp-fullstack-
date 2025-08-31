bffcb5d269f359118bac8d753b13a4d6d18186e3
ismailerguldev <ismailergul.dev@gmail.com>
2025-08-31 12:55:56 +0300

    Express Installed

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+    res.status(200).json({ hi: "hi server" })
+})
+app.listen(PORT, () => {
+    console.log("hi port", PORT)
+})
